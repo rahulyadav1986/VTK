8abdb486d7fbc2b7c31a5f96852694ffcf7d2487
rahulyadav1986 <rahullooksforward@gmail.com>
2020-05-09 17:29:24 +0530

    updated

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+   
+    $('.why-slider').owlCarousel({
+        loop: false,
+        center: true,
+        autoplay: true,
+        margin: 30,
+        smartSpeed: 450,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                dots: false
+            },
+            600: {
+                items: 2
+            },
+            980: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });  
+	
+
+});
+
+window.addEventListener('load', function(){
+    var load_screen = document.getElementById('load_screen');
+    document.body.removeChild(load_screen);
+})
+
+
+
+
